9554471b0fc39d6769bfe3026e4fd3b23d159e19
rxqvx <henriquekenji12@gmail.com>
2023-11-11 22:33:47 -0300

    first commit

---
+++ src/projso/ProjetoSistemasOperacionais.java
@@ -0,0 +1,311 @@
+package projso;
+
+import java.io.FileReader; // biblioteca para ler caracteres de um arquivo de texto
+import java.io.BufferedReader;//biblioteca para leitura do  arquivo de entrada com buffer que resulta numa leitura mais eficiente
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Queue;// biblioteca de fila para facilitar o processo de desenvolvimento contendo os metodos do fifo, ao remover o primeiro a ser inserido será o primeiro a ser removido
+import java.util.LinkedList;// biblioteca necessária para a declaração de queues, segue a politica do fifo sendo eficiente na adição e remoção de elementos no início ou no final da fila, eu poderia usar o arraydeque por ter um desempenho semelhante
+import java.util.ArrayList;//biblioteca apenas para armazenar os tempos de operações IO de um processo, facilita pois contem os metodos de um array
+
+public class ProjetoSistemasOperacionais {
+
+    static class File_writer {
+
+        private String filePath;
+
+        public File_writer(String filePath) {
+            this.filePath = filePath;
+        }
+
+        public void add(String text, boolean isBreakLine) throws IOException {
+            try ( BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+                if (isBreakLine) {
+                    writer.write(text + "\n");
+
+                } else {
+                    writer.write(text);
+
+                }
+            }
+        }
+
+        public void clear() throws IOException {
+            try ( BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                // Não escrever nada no arquivo irá limpar o conteúdo existente.
+            }
+        }
+    }
+
+    static class Processo {
+
+        String pid;
+        int duracao, chegada, cpuTime, quantumTime, queueTime;
+        ArrayList<Integer> operacoesIO;
+
+        public Processo(String pid, int duracao, int chegada, ArrayList<Integer> operacoesIO) {
+            this.pid = pid;
+            this.duracao = duracao;
+            this.chegada = chegada;
+            this.operacoesIO = operacoesIO;
+        }
+
+        private Processo() {
+        }
+
+        public void setDuracao(int duracao) {
+            this.duracao = duracao;
+        }
+
+        public void addCpuTime() {
+            this.cpuTime += 1;
+        }
+
+        public void addQueueTime() {
+            this.queueTime += 1;
+        }
+
+        public void addQuantumTime() {
+            this.quantumTime += 1;
+        }
+
+        public void resetProcessQuantumTime() {
+            this.quantumTime = 0;
+        }
+
+    }
+
+    public static void printQueue(Queue<Processo> Fila, Processo cpu, File_writer writer) throws IOException {
+        if (Fila.isEmpty()) {
+            writer.add("FILA: Nao ha processos na fila", true);
+        } else {
+            writer.add("FILA: ", false);
+            for (Processo process : Fila) {
+                writer.add(process.pid + "(" + process.duracao + ") ", false);
+
+            }
+            writer.add("", true);
+        }
+    }
+
+    public static Processo addToQueue(ArrayList<Processo> processos, Queue<Processo> fila, int chegada) {
+        for (int i = 0; i < processos.size(); i++) {
+            Processo process = processos.get(i);
+
+            if (process.chegada == chegada) {
+                fila.offer(process);
+                return process;
+            }
+        }
+        return null;
+    }
+
+    public static boolean verifyIO(Processo cpu) {
+        cpu.addCpuTime();//adiciona o tempo em que ele fica na cpu
+
+        for (int i = 0; i < cpu.operacoesIO.size(); i++) {
+//            System.out.println(cpu.pid + " " + (cpu.operacoesIO.get(i)) + " " + (cpu.cpuTime));
+            if (cpu.operacoesIO.get(i) < cpu.cpuTime) {
+                cpu.operacoesIO.remove(0);//remove o primeiro elemento do array de operacoes IO
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void roundRobin(int Quantum, ArrayList<Processo> processos) throws IOException, InterruptedException {
+        File_writer writer = new File_writer("C:\\Users\\T-Gamer\\Documents\\NetBeansProjects\\Projeto Sistemas Operacionais\\src\\projso\\saida.txt");
+        Processo cpu = new Processo();
+        Queue<Processo> fila = new LinkedList<>();
+        ArrayList<Integer> auxProcessos = new ArrayList<Integer>();
+
+        for (int w = 0; w < processos.size(); w++) {// para printar as duracoes dos processos no final dos logs
+            auxProcessos.add(processos.get(w).duracao);
+        }
+
+        int tempo = 0;
+
+        writer.clear();
+        writer.add("""
+***********************************
+***** ESCALONADOR ROUND ROBIN *****
+-----------------------------------
+------- INICIANDO SIMULACAO -------
+-----------------------------------
+        """, false);
+
+        writer.add("Quantum: " + Quantum, true);
+
+        addToQueue(processos, fila, 0);//colocando primeiro processo na fila
+
+        System.out.println("Simulacao em tempo real");
+
+        while (true) {
+
+            if (cpu == null && fila.isEmpty()) {
+                writer.add("ACABARAM OS PROCESSOS!!!", true);
+
+                break;
+
+            } else {
+
+                cpu = fila.poll(); // Obter o primeiro processo da fila e consequentemente saindo da fila e virando o processo atual da cpu
+
+                while (cpu != null && cpu.duracao > -1) {
+
+                    writer.add("********** TEMPO " + tempo + " *************", true);
+                    // --------------printar se houve operação io ----------------------
+                    boolean hasOperationIOTime = verifyIO(cpu);//verifica se o processo da cpu recebeu uma operação IO
+                    if (hasOperationIOTime) {
+                        writer.add("#[evento] OPERACAO I/O <" + cpu.pid + ">", true);
+                        // trocar pro proximo processo fila e devolve o processo atual da cpu pra fila
+                        fila.offer(cpu);//devolve pra fila
+                        cpu.resetProcessQuantumTime();
+                        cpu = fila.poll();//colocamos o proximo processo da fila na cpu
+                        cpu.addCpuTime();//adicionamos o tempo total que ele fica na cpu
+
+                    }
+                    // -------------- fim printar se houve operação io ----------------------
+
+                    // --------------printar se houve chegada ----------------------
+                    if (tempo != 0) {//tirando o primeiro processo que eu já adicionei na fila, veremos se chega mais algum no estado de pronto
+
+                        Processo newProcess = addToQueue(processos, fila, tempo);//verifica se algum dos processos chegou na fila
+                        if (newProcess != null) {
+                            writer.add("#[evento] CHEGADA <" + newProcess.pid + ">", true);
+                        }
+                    }
+                    // --------------fim printar se houve chegada ----------------------
+
+                    // --------------printar se a duracao ultrapassou o valor de quantum ----------------------
+                    /* explicando "cpu.resetProcessQuantumTime();". O processo só pode ficar na cpu, 
+                    se o tempo que ele fica na cpu for menor ou igual o valor de quantum, 
+                    como eu devolvi o processo atual pra fila, esse tempo que fica na cpu é resetado e 
+                    a contagem começa novamente quando esse processo voltar pra cpu*/
+                    if (cpu.quantumTime == Quantum) {
+                        writer.add("#[evento] FIM QUANTUM <" + cpu.pid + ">", true);
+                        fila.offer(cpu);//devolve pra fila
+
+                        cpu.resetProcessQuantumTime();
+
+                        cpu = fila.poll();
+
+                    }
+                    // --------------fim printar se a duracao ultrapassou o valor de quantum ----------------------
+                    // --------------printar se houve encerração de processo ----------------------
+                    if (cpu.duracao < 1) {
+                        writer.add("#[evento] ENCERRANDO <" + cpu.pid + ">", true);
+                        cpu.resetProcessQuantumTime();
+
+                        cpu = fila.poll();
+                    }
+                    // --------------fim printar se houve encerração de processo ----------------------
+
+                    //------- printar fila e cpu ---------------------------
+                    printQueue(fila, cpu, writer);
+                    if (cpu != null) {
+                        System.out.print(cpu.pid + " | ");
+                    }
+
+                    Thread.sleep(500);
+                    if (cpu != null) {//se tiver algum processo na cpu printa cpu, se não acabou os processos
+                        writer.add("CPU: " + cpu.pid + "(" + cpu.duracao + ")", true);
+                    }
+
+                    //------- fim printar fila e cpu ---------------------------
+                    tempo++;
+                    if (cpu != null) {
+                        cpu.addQuantumTime();// adiciona a duracao em que o processo fica na cpu, durante essa estadia
+                        cpu.setDuracao(cpu.duracao - 1);//diminui a duracao toda vez que tiver na cpu, se a duracao for menor do que um, remover da cpu 
+                    }
+                    for (Processo processo : fila) {
+                        processo.addQueueTime();//adiciona +1 tempo pros processos que tão na fila, para eu poder calcular posteriormente o tempo de espera médio
+                    }
+                }
+
+            }
+
+        }
+        //---- print gráfico de gantt -------------
+        System.out.println("\n-------- Grafico de Gantt -----------");
+        for (int i = 0; i < auxProcessos.size(); i++) {
+            System.out.print("P" + (i + 1) + " | ");
+
+            for (int j = 0; j < auxProcessos.get(i); j++) {
+                System.out.print("#");
+            }
+
+            System.out.println("");
+        }
+        System.out.println("-------------------------------------");
+
+        //-----------------------
+//------ print tempo de espera médio -------------------
+        int numerator = 0;
+
+        for (Processo processo : processos) {
+            numerator += processo.queueTime;
+        }
+
+        int averageWaitingTime = numerator / processos.size();
+
+        System.out.println("Tempo de espera medio: " + averageWaitingTime);
+//------ fim print tempo de espera médio -------------------
+
+        writer.add("""
+-----------------------------------
+------- Encerrando simulacao ------
+-----------------------------------
+        """, false);
+
+    }
+
+    public static void main(String[] args) {
+        String quantum;
+
+        if (args.length > 0 && !args[0].isEmpty()) {
+            quantum = args[0];
+        } else {
+            quantum = "4";
+        }
+
+        try {
+            ArrayList<Processo> processos = new ArrayList<Processo>();
+            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\T-Gamer\\Documents\\NetBeansProjects\\Projeto Sistemas Operacionais\\src\\projso\\arquivo.txt"));
+            String linha;
+
+            while ((linha = br.readLine()) != null) {
+
+                String[] palavras = linha.split(" ");
+
+                if (palavras.length >= 2) {
+                    String pid = palavras[0];
+                    int duracao = Integer.parseInt(palavras[1]);
+                    int chegada = Integer.parseInt(palavras[2]);
+
+                    ArrayList<Integer> operacoesIO = new ArrayList<Integer>();
+
+                    if (palavras.length >= 4) {
+                        String[] operacoesIOStr = palavras[3].split(",");
+                        for (String operacaoStr : operacoesIOStr) {
+                            operacoesIO.add(Integer.parseInt(operacaoStr));
+                        }
+                    }
+
+                    processos.add(new Processo(pid, duracao, chegada, operacoesIO));
+                }
+
+            }
+
+            roundRobin(Integer.parseInt(quantum), processos);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/projso/README.txt
@@ -0,0 +1,30 @@
+Instruções para rodar o programa
+
+Já no diretório do arquivo ProjetoSistemasOperacionais.java (src>projso>ProjetoSistemasOperacionais.java), é necessário ter um arquivo chamado "arquivo.txt" na qual você inserirá as entradas dos processos no seguinte formato:
+• A primeira palavra mostra o nome do processo (PID);
+• A segunda palavra mostra a duração do processo;
+• A terceira palavra mostra o instante de chegada do processo;
+• Caso haja a quarta palavra, esta indicará se o processo realiza operações de I/O;
+
+Exemplo: 
+P1 9 10 2,4,6,8
+P2 10 4 5
+P3 5 0 2
+P4 7 1 3,6
+P5 2 17
+
+Você pode optar por rodar dentro do Apache Netbeans IDE 13 ou pelo terminal. Caso escolha pelo terminal, você pode escolher o valor de quantum apenas inserindo um número como parâmetro (caso não insira nenhum parâmetro, terá como valor padrão o número 4), veja o exemplo:
+
+para rodar pelo terminal rode os seguintes comandos:
+- javac *.java
+- java ProjetoSistemasOperacionais.java 5
+
+Esse "5" é o parâmetro que define o valor de quantum, certifique-se de substituir para o valor desejado.
+
+para rodar pela IDE:
+- abra o projeto
+- clique com o botão direito do mouse em cima do arquivo ProjetoSistemasOperacionais.java
+- clique em Run File (Shift + F6)
+
+
+em seguida deverá ser gerado
